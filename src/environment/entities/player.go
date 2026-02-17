608c63357e14f3dd106ce2d19460555777e250c4
Noah <kalervo.noah@gmail.com>
2026-02-17 15:25:40 +1100

    fixed incorrect rotation check

--- src/environment/entities/player.go
+++ src/environment/entities/player.go
@@ -19,7 +19,7 @@ func (player *Player) Move(x uint16, y uint16, rotation float32, updateNumber in
 	player.updateNumber = updateNumber
 
 	rotationDiff := player.rotation - rotation
-	if player.x == x && player.y == y && (rotationDiff > 0.04 || rotationDiff < -0.04) {
+	if player.x == x && player.y == y && (rotationDiff < 0.04 && rotationDiff > -0.04) {
 		return
 	}
 
